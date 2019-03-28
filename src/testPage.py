e280f2936f59e25567c35105adc2b2499596a919
LyTuan <lytuanduong96@gmail.com>
2019-03-27 17:38:03 -0700

    push file test Page

--- src/testPage.py
+++ src/testPage.py
@@ -23,23 +23,23 @@ class TestPages(unittest.TestCase):
         page = MainPage(self.driver)
         self.assertTrue(page.check_page_loaded())
 
-    # def test_search_item(self):
-    #     print "\n" + str(test_cases(1))
-    #     page = MainPage(self.driver)
-    #     search_result = page.search_item("pin dell insprison 3537")
-    #     self.assertIn("Nexus 5", search_result)
+    def test_search_item(self):
+        print "\n" + str(test_cases(1))
+        page = MainPage(self.driver)
+        search_result = page.search_item("pin dell insprison 3537")
+        self.assertIn("Nexus 5", search_result)
 
-    # def test_sign_up_button(self):
-    #     print "\n" + str(test_cases(2))
-    #     page = MainPage(self.driver)
-    #     signUpPage = page.click_sign_up_button()
-    #     self.assertIn("user/register", signUpPage.get_url())
+    def test_sign_up_button(self):
+        print "\n" + str(test_cases(2))
+        page = MainPage(self.driver)
+        signUpPage = page.click_sign_up_button()
+        self.assertIn("user/register", signUpPage.get_url())
 
-    # def test_sign_in_button(self):
-    #     print "\n" + str(test_cases(3))
-    #     page = MainPage(self.driver)
-    #     loginPage = page.click_sign_in_button()
-    #     self.assertIn("user/login", loginPage.get_url())
+    def test_sign_in_button(self):
+        print "\n" + str(test_cases(3))
+        page = MainPage(self.driver)
+        loginPage = page.click_sign_in_button()
+        self.assertIn("user/login", loginPage.get_url())
 
     def test_sign_in_with_valid_user(self):
         print "\n" + str(test_cases(4))
@@ -49,13 +49,13 @@ class TestPages(unittest.TestCase):
         time.sleep(20)
         self.assertIn("user/login", result.get_url())
 
-
-    def test_sign_in_with_in_valid_user(self):
-        print "\n" + str(test_cases(5))
-        mainPage = MainPage(self.driver)
-        loginPage = mainPage.click_sign_in_button()
-        result = loginPage.login_with_in_valid_user("invalid_user")
-        self.assertIn("There was a problem with your request", result)
+    #
+    # def test_sign_in_with_in_valid_user(self):
+    #     print "\n" + str(test_cases(5))
+    #     mainPage = MainPage(self.driver)
+    #     loginPage = mainPage.click_sign_in_button()
+    #     result = loginPage.login_with_in_valid_user("invalid_user")
+    #     self.assertIn("There was a problem with your request", result)
 
     def tearDown(self):
         self.driver.close()
