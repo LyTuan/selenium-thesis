8bfc4dc20d343871cc1551ae2c7c85cfd67ea1c0
mss <mss@vcs.com>
2019-04-23 13:34:20 +0700

    Add search side

--- ide-result/katalon/code/testOrderAndDelete.py
+++ ide-result/katalon/code/testOrderAndDelete.py
@@ -9,7 +9,7 @@ import unittest, time, re
 
 class TestOrder(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome("C:\\Users\\admin\\Documents\\tuanlv24\\selenium\\driver\\chromedriver.exe")
         self.driver.implicitly_wait(30)
         self.base_url = "https://www.katalon.com/"
         self.verificationErrors = []
